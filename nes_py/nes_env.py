828b72e93fe71e38bbcb94b2d55a86af83b1de69
guillemdb <guillem@fragile.tech>
2022-04-12 13:45:43 +0200

    Get state data using get_state
    Signed-off-by: guillemdb <guillem@fragile.tech>

--- nes_py/nes_env.py
+++ nes_py/nes_env.py
@@ -425,8 +425,7 @@ class NESEnv(gym.Env):
         """Return a list of actions meanings."""
         return ['NOOP']
 
-    @property
-    def state(self) -> np.ndarray:
+    def get_state(self) -> np.ndarray:
         arr = np.empty(_LIB.StateSize(self._env), dtype=np.byte)
         _LIB.DumpState(self._env, arr.ctypes.data)
         return arr
